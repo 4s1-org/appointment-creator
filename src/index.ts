0f044cbe565e50f463b807f4e4d96de9f8d6f8cc
Steffen <steffen@4s1.de>
2020-11-11 16:04:51 +0100

    Generator

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import { ferienListe } from './ferien-liste'
+import { CalenderItem } from './calendar-item'
+import { Generator } from './generator'
 
 async function main(): Promise<void> {
-  for (const ferien of ferienListe) {
-    for (const woche of ferien.split()) {
-      console.log(woche.beginn.format(), woche.ende.format(), woche.text)
-    }
-  }
+  // for (const ferien of ferienListe) {
+  //   for (const woche of ferien.split()) {
+  //     console.log(woche.beginn.format(), woche.ende.format(), woche.text)
+  //   }
+  // }
+  const item1 = new CalenderItem({ year: 2020, month: 11, day: 11 }, { year: 2020, month: 11, day: 12 }, "Test1", true, "Feiertag")
+  const generator = new Generator()
+  const txt = generator.generate([item1])
+  console.log(txt)
 }
 
 main()
