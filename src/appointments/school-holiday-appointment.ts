fe654ddc1275db11ffdd4b5aac5a25f94be8dddd
Steffen <yellowgarbagebag@4s1.de>
2020-11-12 17:15:04 +0100

    Feiernsplit wieder rein genommen

--- src/appointments/school-holiday-appointment.ts
+++ src/appointments/school-holiday-appointment.ts
@@ -4,4 +4,24 @@ export class SchoolHolidayAppointment extends MultiDayAppointment {
   constructor(begin: string, end: string, text: string) {
     super(begin, end, text, false, 'Ferien')
   }
+
+  public split(): MultiDayAppointment[] {
+    const result: MultiDayAppointment[] = []
+
+    let datum = this._beginn
+    let woche = 1
+
+    while (datum < this._ende) {
+      result.push({
+        beginn: datum,
+        ende: datum.add(6, 'day'),
+        text: `${this.text} (Woche ${woche})`,
+      })
+
+      woche++
+      datum = datum.add(7, 'day')
+    }
+
+    return result
+  }
 }
