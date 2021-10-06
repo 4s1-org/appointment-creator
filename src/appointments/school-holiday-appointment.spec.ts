e3d64d46acff8b7ef4439da028985ba8e2b7e207
Steffen <steffen@4s1.de>
2021-10-06 18:56:05 +0200

    chore: from chai/mocha to jest

--- src/appointments/school-holiday-appointment.spec.ts
+++ src/appointments/school-holiday-appointment.spec.ts
@@ -1,5 +1,3 @@
-import { expect } from 'chai'
-import { describe, it } from 'mocha'
 import { DateUtils } from '../date-utils.js'
 import { SchoolHolidayAppointment } from './school-holiday-appointment.js'
 
@@ -10,11 +8,11 @@ describe('School Holiday', () => {
     // Act
     const split = ferien.split()
     // Assert
-    expect(split.length).to.be.eq(2)
-    expect(DateUtils.format(split[0].begin)).to.be.eq('02.11.2020', 'Beginn erste Woche')
-    expect(DateUtils.format(split[0].end)).to.be.eq('08.11.2020', 'Ende erste Woche')
-    expect(DateUtils.format(split[1].begin)).to.be.eq('09.11.2020', 'Beginn zweite Woche')
-    expect(DateUtils.format(split[1].end)).to.be.eq('15.11.2020', 'Ende zweite Woche')
+    expect(split.length).toEqual(2)
+    expect(DateUtils.format(split[0].begin)).toEqual('02.11.2020') //, 'Beginn erste Woche')
+    expect(DateUtils.format(split[0].end)).toEqual('08.11.2020') //, 'Ende erste Woche')
+    expect(DateUtils.format(split[1].begin)).toEqual('09.11.2020') //, 'Beginn zweite Woche')
+    expect(DateUtils.format(split[1].end)).toEqual('15.11.2020') //, 'Ende zweite Woche')
   })
 
   it('Weihnachtsferien 2021 fangen donnerstags an (Issue #10)', () => {
@@ -23,13 +21,13 @@ describe('School Holiday', () => {
     // Act
     const split = weihnachtsferien.split()
     // Assert
-    expect(split.length).to.be.eq(3)
-    expect(DateUtils.format(split[0].begin)).to.be.eq('23.12.2021', 'Beginn erste Woche')
-    expect(DateUtils.format(split[0].end)).to.be.eq('26.12.2021', 'Ende erste Woche')
-    expect(DateUtils.format(split[1].begin)).to.be.eq('27.12.2021', 'Beginn zweite Woche')
-    expect(DateUtils.format(split[1].end)).to.be.eq('02.01.2022', 'Ende zweite Woche')
-    expect(DateUtils.format(split[2].begin)).to.be.eq('03.01.2022', 'Beginn dritte Woche')
-    expect(DateUtils.format(split[2].end)).to.be.eq('09.01.2022', 'Ende dritte Woche')
+    expect(split.length).toEqual(3)
+    expect(DateUtils.format(split[0].begin)).toEqual('23.12.2021') //, 'Beginn erste Woche')
+    expect(DateUtils.format(split[0].end)).toEqual('26.12.2021') //, 'Ende erste Woche')
+    expect(DateUtils.format(split[1].begin)).toEqual('27.12.2021') //, 'Beginn zweite Woche')
+    expect(DateUtils.format(split[1].end)).toEqual('02.01.2022') //, 'Ende zweite Woche')
+    expect(DateUtils.format(split[2].begin)).toEqual('03.01.2022') //, 'Beginn dritte Woche')
+    expect(DateUtils.format(split[2].end)).toEqual('09.01.2022') //, 'Ende dritte Woche')
   })
 
   it('Osterferien 2021 fangen dienstags an (Issue #9)', () => {
@@ -38,10 +36,10 @@ describe('School Holiday', () => {
     // Act
     const split = osterferien.split()
     // Assert
-    expect(split.length).to.be.eq(2)
-    expect(DateUtils.format(split[0].begin)).to.be.eq('06.04.2021', 'Beginn erste Woche')
-    expect(DateUtils.format(split[0].end)).to.be.eq('11.04.2021', 'Ende erste Woche')
-    expect(DateUtils.format(split[1].begin)).to.be.eq('12.04.2021', 'Beginn zweite Woche')
-    expect(DateUtils.format(split[1].end)).to.be.eq('18.04.2021', 'Ende zweite Woche')
+    expect(split.length).toEqual(2)
+    expect(DateUtils.format(split[0].begin)).toEqual('06.04.2021') //, 'Beginn erste Woche')
+    expect(DateUtils.format(split[0].end)).toEqual('11.04.2021') //, 'Ende erste Woche')
+    expect(DateUtils.format(split[1].begin)).toEqual('12.04.2021') //, 'Beginn zweite Woche')
+    expect(DateUtils.format(split[1].end)).toEqual('18.04.2021') //, 'Ende zweite Woche')
   })
 })
