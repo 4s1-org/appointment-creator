8e97a83ae6b91338cd7dac29cd11d9b6c5bec8cd
Steffen <steffen@4s1.de>
2022-02-13 10:52:26 +0100

    test: switch from jest to ava

--- src/appointments/school-holiday-appointment.spec.ts
+++ src/appointments/school-holiday-appointment.spec.ts
@@ -1,45 +1,44 @@
 import { DateUtils } from '../date-utils'
 import { SchoolHolidayAppointment } from './school-holiday-appointment'
+import test from 'ava'
 
-describe('School Holiday', () => {
-  it('split 2 Wochen', () => {
-    // Arrange
-    const ferien = new SchoolHolidayAppointment('heFe', '02.11.2020', '13.11.2020')
-    // Act
-    const split = ferien.split()
-    // Assert
-    expect(split.length).toEqual(2)
-    expect(DateUtils.format(split[0].begin)).toEqual('02.11.2020') //, 'Beginn erste Woche')
-    expect(DateUtils.format(split[0].end)).toEqual('08.11.2020') //, 'Ende erste Woche')
-    expect(DateUtils.format(split[1].begin)).toEqual('09.11.2020') //, 'Beginn zweite Woche')
-    expect(DateUtils.format(split[1].end)).toEqual('15.11.2020') //, 'Ende zweite Woche')
-  })
+test('split 2 Wochen', (t) => {
+  // Arrange
+  const ferien = new SchoolHolidayAppointment('heFe', '02.11.2020', '13.11.2020')
+  // Act
+  const split = ferien.split()
+  // Assert
+  t.is(split.length, 2)
+  t.is(DateUtils.format(split[0].begin), '02.11.2020') //, 'Beginn erste Woche')
+  t.is(DateUtils.format(split[0].end), '08.11.2020') //, 'Ende erste Woche')
+  t.is(DateUtils.format(split[1].begin), '09.11.2020') //, 'Beginn zweite Woche')
+  t.is(DateUtils.format(split[1].end), '15.11.2020') //, 'Ende zweite Woche')
+})
 
-  it('Weihnachtsferien 2021 fangen donnerstags an (Issue #10)', () => {
-    // Arrange
-    const weihnachtsferien = new SchoolHolidayAppointment('weFe', '23.12.2021', '08.01.2022')
-    // Act
-    const split = weihnachtsferien.split()
-    // Assert
-    expect(split.length).toEqual(3)
-    expect(DateUtils.format(split[0].begin)).toEqual('23.12.2021') //, 'Beginn erste Woche')
-    expect(DateUtils.format(split[0].end)).toEqual('26.12.2021') //, 'Ende erste Woche')
-    expect(DateUtils.format(split[1].begin)).toEqual('27.12.2021') //, 'Beginn zweite Woche')
-    expect(DateUtils.format(split[1].end)).toEqual('02.01.2022') //, 'Ende zweite Woche')
-    expect(DateUtils.format(split[2].begin)).toEqual('03.01.2022') //, 'Beginn dritte Woche')
-    expect(DateUtils.format(split[2].end)).toEqual('09.01.2022') //, 'Ende dritte Woche')
-  })
+test('Weihnachtsferien 2021 fangen donnerstags an (Issue #10)', (t) => {
+  // Arrange
+  const weihnachtsferien = new SchoolHolidayAppointment('weFe', '23.12.2021', '08.01.2022')
+  // Act
+  const split = weihnachtsferien.split()
+  // Assert
+  t.is(split.length, 3)
+  t.is(DateUtils.format(split[0].begin), '23.12.2021') //, 'Beginn erste Woche')
+  t.is(DateUtils.format(split[0].end), '26.12.2021') //, 'Ende erste Woche')
+  t.is(DateUtils.format(split[1].begin), '27.12.2021') //, 'Beginn zweite Woche')
+  t.is(DateUtils.format(split[1].end), '02.01.2022') //, 'Ende zweite Woche')
+  t.is(DateUtils.format(split[2].begin), '03.01.2022') //, 'Beginn dritte Woche')
+  t.is(DateUtils.format(split[2].end), '09.01.2022') //, 'Ende dritte Woche')
+})
 
-  it('Osterferien 2021 fangen dienstags an (Issue #9)', () => {
-    // Arrange
-    const osterferien = new SchoolHolidayAppointment('osFe', '06.04.2021', '16.04.2021')
-    // Act
-    const split = osterferien.split()
-    // Assert
-    expect(split.length).toEqual(2)
-    expect(DateUtils.format(split[0].begin)).toEqual('06.04.2021') //, 'Beginn erste Woche')
-    expect(DateUtils.format(split[0].end)).toEqual('11.04.2021') //, 'Ende erste Woche')
-    expect(DateUtils.format(split[1].begin)).toEqual('12.04.2021') //, 'Beginn zweite Woche')
-    expect(DateUtils.format(split[1].end)).toEqual('18.04.2021') //, 'Ende zweite Woche')
-  })
+test('Osterferien 2021 fangen dienstags an (Issue #9)', (t) => {
+  // Arrange
+  const osterferien = new SchoolHolidayAppointment('osFe', '06.04.2021', '16.04.2021')
+  // Act
+  const split = osterferien.split()
+  // Assert
+  t.is(split.length, 2)
+  t.is(DateUtils.format(split[0].begin), '06.04.2021') //, 'Beginn erste Woche')
+  t.is(DateUtils.format(split[0].end), '11.04.2021') //, 'Ende erste Woche')
+  t.is(DateUtils.format(split[1].begin), '12.04.2021') //, 'Beginn zweite Woche')
+  t.is(DateUtils.format(split[1].end), '18.04.2021') //, 'Ende zweite Woche')
 })
